473ec381f68b3178fe1e84f7b74fb8f6ddf1b06d
Hajatiana <Hajatiana>
2017-08-16 20:24:51 +0200

    V8

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ var connect = require('connect');
 var app      = express();
 var port     = process.env.PORT || 8080;
 
+// Configuration
+app.use(express.static('public'));
+app.use(connect.logger('dev'));
+app.use(connect.json());  
+app.use(connect.urlencoded());
+
+// Routes
+
+require('routes/routes.js')(app);
+
 app.listen(port);
 
 console.log('The App runs on port ' + port);
