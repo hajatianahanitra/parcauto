a6c1b86e60abeafca5e9bea8c4fdc6a3ddf9508e
Hajatiana <Hajatiana>
2017-08-16 20:18:04 +0200

    V3

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 
-/**
- * Module dependencies.
- */
-var express  = require('express');
-var connect = require('connect');
-var app      = express();
-var port     = process.env.PORT || 8080;
-
-
-app.listen(port);
-
-console.log('The App runs on port ' + port);
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+app.listen(process.env.PORT || 8080 , () =>
+ console.log('All is ok! '))
