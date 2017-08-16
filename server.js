83175ce7c21e4f91555a73bc0ba9edc414c87631
Hajatiana <Hajatiana>
2017-08-16 20:21:18 +0200

    V5

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-
+/*
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
 app.listen(process.env.PORT || 8080 , () =>
  console.log('All is ok! '))
+*/
+
+/**
+ * Module dependencies.
+ */
+var express  = require('express');
+var connect = require('connect');
+var app      = express();
+var port     = process.env.PORT || 8080;
+
+// Configuration
+app.use(express.static(__dirname + '/public'));
+app.use(connect.logger('dev'));
+app.use(connect.json());  
+app.use(connect.urlencoded());
+
+// Routes
+
+require('./routes/routes.js')(app);
+
+app.listen(port);
+
+console.log('The App runs on port ' + port);
